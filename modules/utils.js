c2b9ea5f206b70c19b100501597e6eefd19695a0
jchung150 <cloudfan150@gmail.com>
2024-09-26 23:07:21 -0700

    Completed Part 3

--- modules/utils.js
+++ modules/utils.js
@@ -1,5 +1,22 @@
-function getDate() {
-  return new Date().toString();
+const fs = require("fs");
+
+function getDate(name) {
+  let currentDate = new Date().toLocaleString();
+  return `${name}, current date is ${currentDate}`;
 }
 
-module.exports = getDate;
+function writeFile(text, callback) {
+  fs.appendFile("file.txt", text, function (err) {
+    if (err) return callback(err);
+    callback(null, "Text appended to file");
+  });
+}
+
+function readFile(fileName, callback) {
+  fs.readFile(fileName, "utf8", function (err, data) {
+    if (err) return callback(err);
+    callback(null, data);
+  });
+}
+
+module.exports = { getDate, writeFile, readFile };
--- server.js
+++ server.js
@@ -1,18 +1,80 @@
+/*
+  This JavaScript file contains code generated with assistance from ChatGPT,
+  an AI language model developed by OpenAI.
+
+  ChatGPT was used for advice on certain functionalities and structures in
+  this project.
+
+  For more information, visit: https://openai.com/chatgpt
+*/
+
+const https = require("https");
 const http = require("http");
+const fs = require("fs");
 let url = require("url");
-let getDate = require("./modules/utils");
+let { getDate, writeFile, readFile } = require("./modules/utils");
 const messages = require("./en");
 
-http
+// Load SSL certificate and key
+// This will cause an error when running the server locally
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/api.juanchung.net/privkey.pem"),
+  cert: fs.readFileSync(
+    "/etc/letsencrypt/live/api.juanchung.net/fullchain.pem"
+  ),
+};
+
+https
   .createServer(function (req, res) {
     let q = url.parse(req.url, true);
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(
-      `<span style="color: blue">${messages.greet(
-        q.query["name"]
-      )} ${getDate()}</span>`
-    );
-  })
-  .listen(8888);
+    const pathSegments = q.pathname
+      .split("/")
+      .filter((segment) => segment.length > 0);
 
-console.log("Server running at http://localhost:8888");
+    if (pathSegments[0] === "writeFile") {
+      writeFile(q.query["text"], function (err, result) {
+        if (err) {
+          res.writeHead(500, { "Content-Type": "text/html" });
+          res.end("Error writing to the file.");
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(`<h3>${result}</h3>`);
+        }
+      });
+    } else if (pathSegments[0] === "readFile") {
+      const fileName = pathSegments[1];
+      if (!fileName) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("404 Not Found");
+        return;
+      }
+      readFile(fileName, function (err, data) {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.end(`Error: File '${fileName}' not found.`);
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(`<h3>${data}</h3>`);
+        }
+      });
+    } else if (pathSegments[0] === "getDate") {
+      const responseText = `<h3 style="color: blue">${messages.greet(
+        q.query["name"]
+      )} ${getDate(q.query["name"])}</h3>`;
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(responseText);
+    } else {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end("404 Not Found");
+    }
+  })
+  .listen(443);
+
+console.log("Server running at https://api.juanchung.net");
+
+http
+  .createServer((req, res) => {
+    res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+    res.end();
+  })
+  .listen(80);
